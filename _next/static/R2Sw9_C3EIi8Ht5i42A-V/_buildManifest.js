ae1874c8ca3da4ed0c710fd22e81db0b6aaa9dcc
devcjoha <carlajoha.all@gmail.com>
2025-12-03 08:53:41 -0400

    Updates

--- _next/static/R2Sw9_C3EIi8Ht5i42A-V/_buildManifest.js
+++ _next/static/R2Sw9_C3EIi8Ht5i42A-V/_buildManifest.js
@@ -4,7 +4,7 @@ self.__BUILD_MANIFEST = {
     "beforeFiles": [
       {
         "source": "/bookmark-landing//_next/:path+",
-        "destination": "/bookmark-landing/_next/:path+"
+        "destination": "/_next/:path+"
       }
     ],
     "fallback": []
