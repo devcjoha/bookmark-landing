bdd788d9693d346ad1ed4434dab911c5cd1696a6
devcjoha <carlajoha.all@gmail.com>
2025-12-03 08:30:50 -0400

    Updates

--- _next/static/y8Hy4PqeD8Eb1zd_TRTrj/_buildManifest.js
+++ _next/static/y8Hy4PqeD8Eb1zd_TRTrj/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/bookmark-landing//_next/:path+",
-        "destination": "/bookmark-landing/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
